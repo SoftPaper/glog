cf9429a2aec41d710db89ec7bc1688d12a440e92
FengChao <smile.fengchao@gmail.com>
2019-08-17 01:28:52 +0800

    sleep 5s.

--- source/main.cpp
+++ source/main.cpp
@@ -60,7 +60,7 @@ int main(int argc,char* argv[])
 		ILOG << "Error message!";
 		WLOG << "Error message!";
 		ELOG << "Error message!";
-        sleep(3);
+        sleep(5);
 	}
 
 	google::ShutdownGoogleLogging();
